ed4c52d8b896ca0801da83e9c2df48e0767fb628
aditri1912 <84520779+aditri1912@users.noreply.github.com>
2021-07-11 14:44:50 +0530

    Add files via upload

---
+++ iron.js
@@ -0,0 +1,25 @@
+class Iron {
+constructor(x,y){
+    var options = {
+'restitution' : 0.8,
+'friction': 3,
+'density':30,
+    }
+    this.body = bodies.rectangle(x,y,50,50,options)
+    this.width = 80;
+    this.height = 50;
+    world.add (world,this.body)
+}
+ display(){
+ var pos = this.body.position;
+ var angle = this.body.angle;
+ Push ()
+ Translate (this.x , this.y);
+ rotate (angle);
+rectMode (CENTER);
+
+fill ("brown");
+rect (0,0,this.width,this.height);
+Pop ();
+ };
+};
---
+++ stone.js
@@ -0,0 +1,28 @@
+class Stone {
+    constructor( x, y,width,height){
+        var options = {
+            'restitution':0.8,
+            'friction':0.9,
+            'density':12,
+        }
+        this.body = bodies.rectangle( x , y , width , height , options)
+        this.width = width;
+        this.height = height;
+
+        World.add(world , this.body)
+
+    }
+    display(){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+        push ();
+        translate (pos.x , pos.y);
+        rotate (angle);
+        rectMode(CENTER)
+        strokeWeight(4);
+        stroke("black")
+        fill ("black")
+        rect ( x , y , this.width , this.height)
+        pop ();
+    }
+};
